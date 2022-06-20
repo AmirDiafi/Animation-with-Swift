fdabbc5f3f14768fbf705541f4fb5a0d46fbcfa1
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2022-06-20 22:52:48 +0100

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by AmirDiafi on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
